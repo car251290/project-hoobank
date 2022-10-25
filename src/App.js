26b1335d77d2664a152a9776f15d9bd10a2926c1
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-10-24 20:44:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React,{useEffect, useState,useContext,useRef} from 'react';
+import {ThemeContext} from './Components/ThemedButton';
+import Toolbar from './Components/Toolbar';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [add,Setadd] =useState(0);
+  const [isOnline,setIsOnline]=useState(null);
+  const timerStop = useRef(0);
+
+  const themes = {
+    light: {
+      foreground: "#000000",
+      background: "#eeeeee"
+    },
+    dark: {
+      foreground: "#ffffff",
+      background: "#222222"
+    }
+  };
+  const ThemeContext = React.createContext(themes.light);
+ 
+  const startHandler = () => {
+    if (timerStop.current) { return; }
+    timerStop.current = setInterval(() => Setadd(c => c+1), 1000);
+  };
+  const Stophandler = () => {
+    clearInterval(timerStop.current);
+    timerStop.current = 0;
+  }
+
+  useEffect(()=> {
+    document.title = `You clicked ${add} times`;
+
+  })
+  
+  useEffect(() => {
+    return()=> clearInterval(timerStop.current);
+
+  },[])
+
+  useEffect(()=>{
+    function handleStatusChange(status){
+      setIsOnline(status.isOnline);
+    }
+  },[add]);
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p1>Hello you can click {add}</p1>
+     <button onClick={()=> Setadd(add+1)}>+</button>
+     <button onClick={()=> Setadd(add-1)}>-</button>
+     <button onClick={()=> startHandler()}>Start</button>
+     <button onClick={()=> Stophandler()}>Stop</button>
+     <div>
+      <ThemeContext.Provider value={themes.light}>
+      <Toolbar />
+    </ThemeContext.Provider>
     </div>
+   
+    </div>
+   
+   
   );
 }
+
 
 export default App;
