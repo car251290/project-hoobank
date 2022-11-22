7276c9911be9b304ddd77f6a39482d9c38c61dcc
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-11-22 15:38:35 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,69 +1,24 @@
-import React,{useEffect, useState,useContext,useRef} from 'react';
-import {ThemeContext} from './Components/ThemedButton';
-import Toolbar from './Components/Toolbar';
-import './App.css';
+import React, {useState} from 'react';
+import Bookings from './components/Bookings';
+import Meals from './components/Meals';
+import Error from './components/Error';
 
-const App = () => {
-  const [add,Setadd] =useState(0);
-  const [isOnline,setIsOnline]=useState(null);
-  const timerStop = useRef(0);
 
-  const themes = {
-    light: {
-      foreground: "#000000",
-      background: "#eeeeee"
-    },
-    dark: {
-      foreground: "#ffffff",
-      background: "#222222"
-    }
-  };
-  const ThemeContext = React.createContext(themes.light);
- 
-  const startHandler = () => {
-    if (timerStop.current) { return; }
-    timerStop.current = setInterval(() => Setadd(c => c+1), 1000);
-  };
-  const Stophandler = () => {
-    clearInterval(timerStop.current);
-    timerStop.current = 0;
-  }
-
-  useEffect(()=> {
-    document.title = `You clicked ${add} times`;
-
-  })
-  
-  useEffect(() => {
-    return()=> clearInterval(timerStop.current);
-
-  },[])
-
-  useEffect(()=>{
-    function handleStatusChange(status){
-      setIsOnline(status.isOnline);
-    }
-  },[add]);
-
- 
-  return (
-    <div className="App">
-     <p1>Hello you can click {add}</p1>
-     <button onClick={()=> Setadd(add+1)}>+</button>
-     <button onClick={()=> Setadd(add-1)}>-</button>
-     <button onClick={()=> startHandler()}>Start</button>
-     <button onClick={()=> Stophandler()}>Stop</button>
-     <div>
-      <ThemeContext.Provider value={themes.light}>
-      <Toolbar />
-    </ThemeContext.Provider>
-    </div>
-   
-    </div>
-   
-   
-  );
+const App = ({handleButtonClicked,handleInputChange,invalidHackers}) => {
+      
+        return (
+            
+            <div className="container-fluid">
+            <center>
+                <h2>Hacker Hostel</h2>
+            </center>
+            <div className="container">
+                <Bookings handleInputChange={handleInputChange} handleButtonClicked={handleButtonClicked}></Bookings>
+                <Error invalidHackers={invalidHackers}></Error>
+                <Meals></Meals>
+            </div>
+        </div>
+        )
 }
-
 
 export default App;
